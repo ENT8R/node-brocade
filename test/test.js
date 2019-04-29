8d10b86cb361f7a0fa6524b79d94273e12034f5f
ENT8R <info.ent8r@gmail.com>
2019-04-29 15:45:37 +0200

    Fix an issue with the 'add' function which didn't work properly when no item exists yet for the barcode, some other small improvements and dependency updates

--- test/test.js
+++ test/test.js
@@ -8,12 +8,10 @@ const expect = chai.expect;
 // const should = chai.should();
 
 const datakick = require('../index.js');
-
-const Helper = require('../lib/helper.js');
-
+const request = require('../lib/request.js');
 
 describe('datakick', function() {
-  this.slow(1500);
+  this.slow(2000);
 
   describe('#item()', () => {
     it('should return the data of an item without error', (done) => {
@@ -227,11 +225,11 @@ describe('datakick', function() {
 });
 
 describe('Helper', function() {
-  this.slow(1500);
+  this.slow(2000);
 
   describe('#request', () => {
     it('should return an error when the options object is not present', () => {
-      return Helper.request().then(() => {
+      return request().then(() => {
         return Promise.reject(new Error('Expected method to reject.'));
       }).catch((error) => {
         expect(error).to.be.an('error');
@@ -242,7 +240,7 @@ describe('Helper', function() {
 
   describe('#request', () => {
     it('should return an error when the page could not be loaded', () => {
-      return Helper.request({
+      return request({
         hostname: 'www.datakick.org',
         path: '/items/api/000000000000?version=1',
         method: 'GET'
@@ -257,7 +255,7 @@ describe('Helper', function() {
 
   describe('#request', () => {
     it('should return an error when the page does not exist', () => {
-      return Helper.request({
+      return request({
         hostname: 'www.datakik.org',
         path: '/api/items/000000000000?version=1',
         method: 'GET'
